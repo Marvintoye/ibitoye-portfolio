fb2890dbf2e1fd540316de1368e115d3d58d72cc
Marvintoye <ibitoyemarvellous2@gmail.com>
2026-03-12 13:22:30 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,60 +1,55 @@
-// smooth scroll
+// 3D tilt
 
-const lenis = new Lenis()
-
-function raf(time) {
-lenis.raf(time)
-requestAnimationFrame(raf)
-}
-
-requestAnimationFrame(raf)
-
-
-// tilt cards
-
-VanillaTilt.init(document.querySelectorAll(".tilt"),{
+VanillaTilt.init(document.querySelectorAll(".tilt"), {
 max:15,
 speed:400
-})
+});
 
 
 // modal
 
 function openModal(){
-document.getElementById("modal").style.display="block"
+document.getElementById("modal").style.display="block";
 }
 
 function closeModal(){
-document.getElementById("modal").style.display="none"
+document.getElementById("modal").style.display="none";
 }
 
 
-// github projects
+// dark mode
+
+const toggle = document.getElementById("themeToggle");
+
+toggle.onclick = () => {
+document.body.classList.toggle("light");
+}
+
+
+// GitHub project loader
 
 fetch("https://api.github.com/users/YOURUSERNAME/repos")
 
-.then(res=>res.json())
+.then(res => res.json())
 
-.then(data=>{
+.then(data => {
 
-let container=document.getElementById("githubProjects")
+let container = document.getElementById("projectsContainer");
 
-data.slice(0,6).forEach(repo=>{
+data.slice(0,6).forEach(repo => {
 
-let div=document.createElement("div")
+let div = document.createElement("div");
 
-div.innerHTML=`
+div.classList.add("repo");
 
+div.innerHTML = `
 <h3>${repo.name}</h3>
+<p>${repo.description || "No description"}</p>
+<a href="${repo.html_url}" target="_blank">View Repo</a>
+`;
 
-<p>${repo.description}</p>
+container.appendChild(div);
 
-<a href="${repo.html_url}">View Repo</a>
+});
 
-`
-
-container.appendChild(div)
-
-})
-
-})
+});
